fc77d07581b468239cf81cc78fc94fadcee4105f
janosch <janosch@spleen.de>
2016-05-24 14:27:48 +0200

    no more timeout

--- js/cropfield.js
+++ js/cropfield.js
@@ -1,5 +1,5 @@
 jQuery().ready(function($){
-
+	
 	var boxWidth = $('#Dropzone').data('boxwidth');
 	var boxHeight = $('#Dropzone').data('boxheight');
 	var width = $('#Dropzone').data('width');
@@ -9,7 +9,6 @@ jQuery().ready(function($){
 	var maxWidth = $('#Dropzone').data('maxwidth');
 	var maxHeight = $('#Dropzone').data('maxheight');
 	var aspect = $('#Dropzone').data('aspect');
-	
 	if(boxWidth == 0){
 		boxWidth = $('#Dropzone').width();
 		//boxHeight = 0;
@@ -22,42 +21,42 @@ jQuery().ready(function($){
 			this.removeFile(this.files[0]);
 		}
 	});
-	//console.log("boxwidth: "+boxWidth);
-	$('#Dropzone').data('dropzoneInterface').backend.on('complete',function(){
-		setTimeout(function(){
-			var pic_real_width = document.querySelector('#cropArea').naturalWidth;
-			var pic_real_height = document.querySelector('#cropArea').naturalHeight;
-			//console.log("realW: "+pic_real_width+" realH: "+pic_real_height);
-			if(pic_real_width < minWidth || pic_real_height < minHeight){
-				$('#Dropzone').data('dropzoneInterface').clear();
-				alert("Uploaded Image is too Small. must have at least "+minWidth+"x"+minHeight+"px");
-			}
-			//var boxHeight = boxWidth/pic_real_width*pic_real_height;
-			//boxHeight = boxWidth/pic_real_width*pic_real_height;
-			if(pic_real_width > pic_real_height){
-				var offsetX = (pic_real_width/2) - (pic_real_height/2);
-				var offsetY = 0;
-			} else{
-				var offsetX = 0;
-				var offsetY = (pic_real_height/2) - (pic_real_width/2);;
-			}
-			$('#cropArea').css("width","auto");
-			$('.jcrop-holder').css("width","100%");
-			$('#cropArea').Jcrop({
-				onChange:   setCoords,
-				onSelect:   setCoords,
-				onRelease:  clearCoords,
-				setSelect:   [ offsetX, offsetY, pic_real_width, pic_real_height],
-				minSize:  [minWidth, minHeight],
-				maxSize: [maxWidth, maxHeight],
-				boxWidth: boxWidth,
-				boxHeight: boxHeight,
-				aspectRatio: aspect
-			},function(){
-				jcrop_api = this;
-			})
-			//console.log("bw: "+boxWidth+" bh: "+boxHeight);
-		},1000);
+	$('#Dropzone').data('dropzoneInterface').backend.on('error',function(errorMessage ){
+		console.log("errorMessage : "+errorMessage);
+	});
+	$('#Dropzone').data('dropzoneInterface').backend.on('thumbnail',function(dataUrl){
+		var pic_real_width = dataUrl.width;
+		var pic_real_height = dataUrl.height;
+		//console.log("realW: "+pic_real_width+" realH: "+pic_real_height);
+		if(pic_real_width < minWidth || pic_real_height < minHeight){
+			$('#Dropzone').data('dropzoneInterface').clear();
+			alert("Uploaded Image is too Small. must have at least "+minWidth+"x"+minHeight+"px");
+		}
+		//var boxHeight = boxWidth/pic_real_width*pic_real_height;
+		//boxHeight = boxWidth/pic_real_width*pic_real_height;
+		if(pic_real_width > pic_real_height){
+			var offsetX = (pic_real_width/2) - (pic_real_height/2);
+			var offsetY = 0;
+		} else{
+			var offsetX = 0;
+			var offsetY = (pic_real_height/2) - (pic_real_width/2);;
+		}
+		$('#cropArea').css("width","auto");
+		$('.jcrop-holder').css("width","100%");
+		$('#cropArea').Jcrop({
+			onChange:   setCoords,
+			onSelect:   setCoords,
+			onRelease:  clearCoords,
+			setSelect:   [ offsetX, offsetY, pic_real_width, pic_real_height],
+			minSize:  [minWidth, minHeight],
+			maxSize: [maxWidth, maxHeight],
+			boxWidth: boxWidth,
+			boxHeight: boxHeight,
+			aspectRatio: aspect
+		},function(){
+			jcrop_api = this;
+		})
+		//console.log("bw: "+boxWidth+" bh: "+boxHeight);
 	});
 });
 
